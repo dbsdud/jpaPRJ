24538eda79d698468d4ed370c4d11082fecbde8e
unknown <duddl425@gmail.com>
2021-05-20 11:51:27 +0900

    first commit

---
+++ src/test/java/com/example/jpaprj/JpaPrjApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jpaprj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaPrjApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
